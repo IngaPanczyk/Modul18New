50caf6eff0b7cc23804af06dd0e9efcc3ae4fd56
IngaPanczyk <ingawaaa@gmail.com>
2020-10-04 22:23:42 +0200

    rozpoczęcie modułu 19, dodanie Mappera i DbService

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,12 +2,15 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor //Dodaje settery i konstruktor
+@NoArgsConstructor
 
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
 }
